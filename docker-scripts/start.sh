9a9c4297b846ed49c1572c2d4448fa77b1b70781
ogunes <ogunes@ebi.ac.uk>
2024-04-08 15:07:31 +0100

    index controller path  added

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -8,15 +8,32 @@ if [ -z "${EBI_PROXY}" ]; then
   java -Djava.security.egd=file:/dev/./urandom -jar app.jar \
     --server.port="${port}" --spring.profiles.active="$profile"
 else
-  java -Djava.security.egd=file:/dev/./urandom \
-    -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk \
-    -Dhttps.proxyPort=3128 \
-    -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk \
-    -Dhttp.proxyPort=3128 \
-    -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
-    -Dftp.proxyHost=hx-wwwcache.ebi.ac.uk \
-    -Dftp.proxyPort=3128 \
-    -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
-    -jar app.jar \
-    --server.port="${port}" --spring.profiles.active="$profile"
+  if [ -z "${HX_PROXY}" ]; then
+
+    java -Djava.security.egd=file:/dev/./urandom \
+      -Dhttps.proxyHost=hh-wwwcache.ebi.ac.uk \
+      -Dhttps.proxyPort=3128 \
+      -Dhttp.proxyHost=hh-wwwcache.ebi.ac.uk \
+      -Dhttp.proxyPort=3128 \
+      -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+      -Dftp.proxyHost=hh-wwwcache.ebi.ac.uk \
+      -Dftp.proxyPort=3128 \
+      -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+      -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
+      --server.port="${port}" --spring.profiles.active=internal
+
+  else
+
+    java -Djava.security.egd=file:/dev/./urandom \
+      -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk \
+      -Dhttps.proxyPort=3128 \
+      -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk \
+      -Dhttp.proxyPort=3128 \
+      -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+      -Dftp.proxyHost=hx-wwwcache.ebi.ac.uk \
+      -Dftp.proxyPort=3128 \
+      -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+      -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
+      --server.port="${port}" --spring.profiles.active=internal
+  fi
 fi
