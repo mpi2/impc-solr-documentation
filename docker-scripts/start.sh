57d4f4eec35b5182e71bbd02a366a78d483f556e
ogunes <ogunes@ebi.ac.uk>
2024-04-09 14:35:25 +0100

    index controller path  added

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 set -e
+[[ -z "${PORT}" ]] && port=8080 || port="${PORT}"
 
-# Determine the server port
-port=${PORT:-8080}
-profile="docker"
 
-# Set common proxy settings
-proxyHostSuffix="wwwcache.ebi.ac.uk"
-proxyPort=3128
-nonProxyHosts="*.ebi.ac.uk|localhost|127.0.0.1"
-
-# Use a different proxy prefix based on HX_PROXY environment variable
 if [ -z "${HX_PROXY}" ]; then
-    proxyPrefix="hh"
+  java -Djava.security.egd=file:/dev/./urandom \
+    -Dhttps.proxyHost=hh-wwwcache.ebi.ac.uk \
+    -Dhttps.proxyPort=3128 \
+    -Dhttp.proxyHost=hh-wwwcache.ebi.ac.uk \
+    -Dhttp.proxyPort=3128 \
+    -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+    -Dftp.proxyHost=hh-wwwcache.ebi.ac.uk \
+    -Dftp.proxyPort=3128 \
+    -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+    -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
+    --server.port="${port}" --spring.profiles.active=docker
 else
-    proxyPrefix="hx"
+  java -Djava.security.egd=file:/dev/./urandom \
+    -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk \
+    -Dhttps.proxyPort=3128 \
+    -Dhttp.proxyHost=hx-wwwcache.ebi.ac.uk \
+    -Dhttp.proxyPort=3128 \
+    -Dhttp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+    -Dftp.proxyHost=hx-wwwcache.ebi.ac.uk \
+    -Dftp.proxyPort=3128 \
+    -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
+    -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
+    --server.port="${port}" --spring.profiles.active="docker
 fi
-
-# Construct the full proxy hosts
-httpsProxyHost="${proxyPrefix}-${proxyHostSuffix}"
-httpProxyHost="${proxyPrefix}-${proxyHostSuffix}"
-ftpProxyHost="${proxyPrefix}-${proxyHostSuffix}"
-
-# Execute the Java application with the specified settings
-java -Djava.security.egd=file:/dev/./urandom \
-     -Dhttps.proxyHost="${httpsProxyHost}" \
-     -Dhttps.proxyPort="${proxyPort}" \
-     -Dhttp.proxyHost="${httpProxyHost}" \
-     -Dhttp.proxyPort="${proxyPort}" \
-     -Dhttp.nonProxyHosts="${nonProxyHosts}" \
-     -Dftp.proxyHost="${ftpProxyHost}" \
-     -Dftp.proxyPort="${proxyPort}" \
-     -Dftp.nonProxyHosts="${nonProxyHosts}" \
-     -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
-     --server.port="${port}" --spring.profiles.active="${profile}"
