cede9685c541b17b932886c993c04068434d2503
ogunes <ogunes@ebi.ac.uk>
2024-04-19 09:17:40 +0100

    null fields added

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -12,7 +12,7 @@ if [ -z "${HX_PROXY}" ]; then
     -Dftp.proxyPort=3128 \
     -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
     -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
-    --server.port=8091 --spring.profiles.active=docker
+    --server.port=8080 --spring.profiles.active=docker
 else
   java -Djava.security.egd=file:/dev/./urandom \
     -Dhttps.proxyHost=hx-wwwcache.ebi.ac.uk \
@@ -24,5 +24,5 @@ else
     -Dftp.proxyPort=3128 \
     -Dftp.nonProxyHosts=*.ebi.ac.uk\|localhost\|127.0.0.1 \
     -jar /home/impc-solr-documentation/impc-solr-documentation-*.jar \
-    --server.port=8091 --spring.profiles.active=docker
+    --server.port=8080 --spring.profiles.active=docker
 fi
